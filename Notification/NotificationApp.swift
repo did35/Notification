038c6e5771e833b3a0987b3c035c440f6d762c2a
Didier <did35@yahoo.com>
2024-05-06 19:26:02 +0200

    Initial Commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Didier Delhaisse on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
